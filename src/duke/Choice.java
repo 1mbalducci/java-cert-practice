a37ed358322e47e5f98610c9c194b5d33de0b9af
Michelle Balducci <michelle.balducci@gmail.com>
2023-07-23 14:49:22 -0500

    simple print

---
+++ src/duke/Choice.java
@@ -0,0 +1,8 @@
+package duke;
+
+public class Choice {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Duke Choice Shop");
+        System.out.println("Hi");
+    }
+}
