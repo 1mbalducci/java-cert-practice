3142a989a68f1983c2fc8dc0825c5859207c82ae
Michelle Balducci <michelle.balducci@gmail.com>
2023-07-26 12:11:20 -0500

    order development

--- src/duke/Order.java
+++ src/duke/Order.java
@@ -1,9 +1,10 @@
 package duke;
 
+import java.util.List;
 import java.util.function.Function;
 
 public class Order {
     static void addChoice(Customer customer){
-    customer.add(item1);
+    customer.choices.add("salad, apple, pie");
     }
 }
--- src/duke/ShopApp.java
+++ src/duke/ShopApp.java
@@ -4,7 +4,7 @@ public class ShopApp {
     public static void main (String[] args) {
         Customer c1 = new Customer();
         c1.name= "pinky";
-        System.out.println(c1.name);
+        System.out.println( c1.choices);
         System.out.println("Hi");
     }
 }
