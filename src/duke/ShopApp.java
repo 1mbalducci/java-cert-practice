b65c0a5469edcfce5f4345a474fd752cd6d59e8d
Michelle Balducci <michelle.balducci@gmail.com>
2023-07-31 18:15:59 -0500

    for each

--- src/duke/ShopApp.java
+++ src/duke/ShopApp.java
@@ -1,17 +1,17 @@
 package duke;
 
 public class ShopApp {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Customer newCustomer = new Customer();
         newCustomer.name = "Pinky";
-        int measurement= 3;
+        int measurement = 3;
 
-        switch (measurement){
-            case 1,2,3:
-                newCustomer.size= "S";
+        switch (measurement) {
+            case 1, 2, 3:
+                newCustomer.size = "S";
                 break;
-            case 7,8,9:
-                newCustomer.size= "L";
+            case 7, 8, 9:
+                newCustomer.size = "L";
                 break;
             default:
                 newCustomer.size = "M";
@@ -19,26 +19,64 @@ public class ShopApp {
         }
 
 
-
         Double tax = .2;
 
-        Clothing item1= new Clothing();
-        Clothing item2= new Clothing();
-        item1.description= "Blue Jacket";
-        item1.price= 20.9;
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+        Clothing item3 = new Clothing();
+        Clothing item4 = new Clothing();
 
-        item2.description= "Orange T-shirt";
-        item2.price= 10.5;
-        item2.size= "S";
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
 
-        Double totalItem1 = item1.price * (1+tax);
-        Double totalItem2 = item2.price * (1+tax);
+        item2.description = "Orange T-shirt";
+        item2.price = 10.5;
+        item2.size = "S";
 
-        System.out.println("Item1 is a "+ item1.description + " that costs $"+ totalItem1 + " with tax.");
-        System.out.println("Item2 is a "+ item2.description + " that costs $"+ totalItem2+".");
-       System.out.println(newCustomer.name + " is size "+newCustomer.size
-       );
+        item3.description = "Green Scarf";
+        item3.price = 5.0;
+        item3.size = "S";
+
+        item4.description = "Blue T-Shirt";
+        item4.price = 10.5;
+        item4.size = "S";
+
+        Clothing[] items = {item1, item2, item3, item4};
+
+        Double totalItem1;
+//        Double totalItem2 = item2.price * (1+tax);
+        Double total = 0.0;
+        String detailsItem;
+
+        for (Clothing item : items) {
+           if (item.size.equals(newCustomer.size)) {
+               System.out.println(item.price);
+               item.price = item.price * (1 + tax);
+               System.out.println(item.price);
+               if (total + item.price < 15) {
+                   total += item.price;
+                   break;
+               }
+           }
+            System.out.println(total);
+
+
+
+            System.out.println(item.description+ " costs $"+item.price +" with tax and is size " + item.size);
+        }
+
+
+//       System.out.println("Item1 is a "+detailsItem +" that costs $"+total +" with tax.");
+//       System.out.println("Item2 is a "+item2.description +" that costs $"+total +".");
+//       System.out.println(newCustomer.name +" is size "+newCustomer.size
+//       );
+        System.out.println("Total =" + total);
     }
 
-
 }
+
+
+
+
+
