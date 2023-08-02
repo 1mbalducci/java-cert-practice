8f79e1963725d994bbe1b2140b93446f4c7de042
Michelle Balducci <michelle.balducci@gmail.com>
2023-08-01 19:45:49 -0500

    getters  and setters

--- src/duke/Clothing.java
+++ src/duke/Clothing.java
@@ -1,8 +1,36 @@
 package duke;
 
 public class Clothing {
-    String description;
-    Double price;
-    String size= "M";
+    public String description;
+    public Double price;
+    public String size= "M";
 
+    public final Double minPrice = 10.00;
+    public final double taxRate =.20;
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price*(1+taxRate);
+    }
+
+    public void setPrice(Double price) {
+
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
